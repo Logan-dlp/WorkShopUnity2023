6722231a497a9e0487337e89866f727065a42429
Logan <pix.59700@gmail.com>
2023-11-14 19:38:45 +0100

    Add camera follow player

--- Assets/_Core/Scripts/PlayerController.cs
+++ Assets/_Core/Scripts/PlayerController.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,12 +5,14 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : MonoBehaviour
 {
+    private Camera _mainCamera;
     private NavMeshAgent _navMeshAgent;
     private Vector3 _mousePosition;
     private Vector3 _targetPosition;
 
     private void Start()
     {
+        _mainCamera = Camera.main;
         _navMeshAgent = GetComponent<NavMeshAgent>();
         if (_targetPosition == null)
         {
@@ -23,6 +22,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        UpdateCameraPosition();
         SetTargetPosition();
         _navMeshAgent.SetDestination(_targetPosition);
     }
@@ -31,14 +31,20 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit,  1000))
+            if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit,  1000))
             {
                 if (hit.collider.gameObject.TryGetComponent(out NavMeshSurface floorMeshSurface))
                 {
                     _targetPosition = hit.point;
-                    Debug.Log(_targetPosition);
                 }
             }
         }
+    }
+
+    void UpdateCameraPosition()
+    {
+        _mainCamera.transform.position = new Vector3(transform.position.x, 
+            _mainCamera.transform.position.y, 
+            transform.position.z);
     }
 }
