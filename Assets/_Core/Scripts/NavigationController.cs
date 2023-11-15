4aca79711b4453eaa5ef9fb8b44566e833624937
Logan <pix.59700@gmail.com>
2023-11-15 11:02:58 +0100

    Change setdestination in click

--- Assets/_Core/Scripts/NavigationController.cs
+++ Assets/_Core/Scripts/NavigationController.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,33 +9,27 @@ public class NavigationController : MonoBehaviour
     private Camera _mainCamera;
     private NavMeshAgent _navMeshAgent;
     private Vector3 _mousePosition;
-    private Vector3 _targetPosition;
 
     private void Start()
     {
         _mainCamera = Camera.main;
         _navMeshAgent = GetComponent<NavMeshAgent>();
-        if (_targetPosition == null)
-        {
-            _targetPosition = transform.position;
-        }
     }
 
     private void Update()
     {
         SetTargetPosition();
-        _navMeshAgent.SetDestination(_targetPosition);
     }
 
     void SetTargetPosition()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit,  1000))
+            if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit,  Single.PositiveInfinity))
             {
                 if (hit.collider.gameObject.TryGetComponent(out NavMeshSurface floorMeshSurface))
                 {
-                    _targetPosition = hit.point;
+                    _navMeshAgent.SetDestination(hit.point);
                 }
             }
         }
