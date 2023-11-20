1618ffe51c7c655c5c8f56c4fb7ca1ab1d7182df
Logan <pix.59700@gmail.com>
2023-11-20 13:06:00 +0100

    Inventory is displayed

--- Assets/_Core/Scripts/PlayerInventory.cs
+++ Assets/_Core/Scripts/PlayerInventory.cs
@@ -1,10 +1,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(NavigationController))]
 public class PlayerInventory : MonoBehaviour
 {
+    [SerializeField] private GameObject _canvasInventory;
     [SerializeField] private int _maxObjectInList = 10;
+    private NavigationController _navigationController;
     private List<GameObject> _inventoryList = new List<GameObject>();
+    private bool _isDisplay = false;
+
+    private void Start()
+    {
+        _navigationController = GetComponent<NavigationController>();
+        _canvasInventory.SetActive(_isDisplay);
+    }
+
+    private void Update()
+    {
+        DisplayInventory();
+    }
+
+    private void DisplayInventory()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _navigationController.enabled = _isDisplay;
+            _isDisplay = !_isDisplay;
+            _canvasInventory.SetActive(_isDisplay);
+        }
+    }
     
     public bool AddObjectInIventory(GameObject gameObject)
     {
